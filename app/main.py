13b829caebb1e5e0b1fdc0602cb8fe3b23812c1e
Muhammad Suleman <muhammadsuleman964@gmail.com>
2021-07-02 12:30:05 +0500

    Allhamdullilah, uploads path error solved 🍕

--- app/main.py
+++ app/main.py
@@ -1,26 +1,36 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource, reqparse
-import werkzeug
+import os
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+
 import model_handling as mh
 
+UPLOAD_FOLDER = 'E:\\UOG\\FYP_Project\\Flask_API_for_Model_Tensorflow_Serving\\uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
-api = Api(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-predict_post_arg = reqparse.RequestParser()
-predict_post_arg.add_argument('file',
-type=werkzeug.datastructures.FileStorage,
-location='files',help='kindly attach image')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-class FYP_Model(Resource):
-    def post(self):
-        arg = predict_post_arg.parse_args()
-        image_file = arg['file']
-        img_path = "app/uploads/images/image.jpg"
-        image_file.save(img_path)
-        result = mh.predict(img_path)
-        return result
+@app.route('/predict/',methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return 'No selected file'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(save_path)
+            result = mh.predict(save_path)
+            return result
 
-api.add_resource(FYP_Model,'/predict/')
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
